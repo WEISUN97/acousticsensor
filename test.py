d9bca10fcc7f3b383aafb6a19cf910824eca4f08
WEISUN97 <wsun7@binghamton.edu>
2025-02-11 15:46:45 -0600

    feat: die design

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
 import gdsfactory as gf
-import Tools.geo_trans as tg
 
-# Create a blank component (essentially an empty GDS cell with some special features)
-c = gf.Component()
-
-tg.polygon([(0, 0), (10, 0), (10, 10), (0, 10)])
+a = [1] * 10
+print(a)
